4b27db7b254e81619d681817d7a8105617518d56
manley <manleytalk@hotmail.com>
2019-03-25 21:32:16 +0800

    分支与循环

--- LeftOrRightDemo/Program.cs
+++ LeftOrRightDemo/Program.cs
@@ -6,7 +6,38 @@ namespace LeftOrRightDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //int a = 12;
+            //int b = 3;
+            //int c = a % b;
+            //if (c == 0)
+            //{
+            //    Console.WriteLine("这个数能被三整除");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("这个数不能能被三整除");
+            //}
+            //int d = 0;
+            //while (d <= 20)
+            //{
+            //    if(d%3==0)
+            //    {
+            //        Console.WriteLine(d);
+            //    }
+
+            //    d++;
+            //}
+
+            for (int i = 0; i <= 20; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.ReadKey();
+            
         }
     }
 }
